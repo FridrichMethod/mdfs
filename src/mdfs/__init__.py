7d283e466b7c81656219fdd5cd35f428e71dd8b6
FridrichMethod <zhaoyangli@stanford.edu>
2026-06-20 13:33:41 -0700

    refactor: drop license-incompatible/superseded modules, export public API
    Remove AlphaFold-derived common/ (CC-BY-NC-SA, unused) and the superseded
    bond-graph topology.py. Add ffxml/__init__.py so package data ships correctly,
    and re-export the public API from mdfs/__init__.py.

--- src/mdfs/__init__.py
+++ src/mdfs/__init__.py
@@ -1,5 +1,86 @@
-"""MD from scratch"""
+"""mdfs: molecular dynamics from scratch (JAX, differentiable forces)."""
+
+from mdfs.constants import BOLTZMANN_KJ_PER_MOL_K, ONE_4PI_EPS0
+from mdfs.energy import (
+    BondedSet,
+    DSFParams,
+    LJMixParams,
+    NonbondedSet,
+    bonded_energy,
+    nonbonded_energy,
+    total_energy_fn,
+)
+from mdfs.integrators import (
+    LangevinParams,
+    State,
+    kinetic_energy,
+    langevin_baoab,
+    maxwell_boltzmann_velocities,
+    temperature,
+    velocity_verlet,
+)
+from mdfs.io import (
+    EnergyLogger,
+    TrajectoryRecorder,
+    combine_callbacks,
+    mdtraj_topology_from_openmm,
+)
+from mdfs.minimize import MinimizationResult, minimize_energy, steepest_descent
+from mdfs.params import (
+    SystemParams,
+    extract_system_params,
+    prepare_topology,
+    system_params_from_pdb,
+    to_bonded_set,
+    to_nonbonded_set,
+)
+from mdfs.partition import all_pairs, neighbor_list
+from mdfs.simulate import make_energy_fn, run, simulate_langevin, simulate_nve
+from mdfs.space import free, periodic, wrap
+from mdfs.utils import configure_logging
 
 __version__ = "0.1.0"
 __author__ = "Zhaoyang Li"
 __email__ = "zhaoyangli@stanford.edu"
+
+__all__ = [
+    "BOLTZMANN_KJ_PER_MOL_K",
+    "ONE_4PI_EPS0",
+    "BondedSet",
+    "DSFParams",
+    "EnergyLogger",
+    "LJMixParams",
+    "LangevinParams",
+    "MinimizationResult",
+    "NonbondedSet",
+    "State",
+    "SystemParams",
+    "TrajectoryRecorder",
+    "all_pairs",
+    "bonded_energy",
+    "combine_callbacks",
+    "configure_logging",
+    "extract_system_params",
+    "free",
+    "kinetic_energy",
+    "langevin_baoab",
+    "make_energy_fn",
+    "maxwell_boltzmann_velocities",
+    "mdtraj_topology_from_openmm",
+    "minimize_energy",
+    "neighbor_list",
+    "nonbonded_energy",
+    "periodic",
+    "prepare_topology",
+    "run",
+    "simulate_langevin",
+    "simulate_nve",
+    "steepest_descent",
+    "system_params_from_pdb",
+    "temperature",
+    "to_bonded_set",
+    "to_nonbonded_set",
+    "total_energy_fn",
+    "velocity_verlet",
+    "wrap",
+]
---
+++ src/mdfs/ffxml/__init__.py
@@ -0,0 +1,1 @@
+"""Bundled force-field XML files (shipped as package data)."""
